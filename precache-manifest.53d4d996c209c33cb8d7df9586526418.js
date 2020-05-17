7c900ed78e77af2759b2abb1ef97339ad97b6bf2
F3n1xLaurent <f3n1xfsociety@mail.ru>
2020-05-17 20:32:07 +0300

    First test

---
+++ precache-manifest.53d4d996c209c33cb8d7df9586526418.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "613cc4c4f7a1549cb8f1dbd23335b062",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5aefe7d336424434f5b3",
+    "url": "/static/js/2.b10c2a3d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.b10c2a3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9a41ab6c8e19e1d50c4",
+    "url": "/static/js/main.0f4ebc19.chunk.js"
+  },
+  {
+    "revision": "92273b49d3690110aa39",
+    "url": "/static/js/runtime-main.584e3335.js"
+  }
+]);
